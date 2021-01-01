692abb4304bb99b5900852ebcf8b6b312db631ce
j4sonshu <jasonshu@terpmail.umd.edu>
2021-01-01 14:55:14 -0500

    new icon

--- src/pages/Lobby.js
+++ src/pages/Lobby.js
@@ -2,12 +2,15 @@ import React from "react";
 import { Container } from "react-bootstrap";
 
 import "./lobby.scss";
+import icon from "../assets/icon.svg";
 
 // Lobby is the parent component. Home and Room are the children components.
 const Lobby = (props) => {
     return (
         <Container className="lobby-area">
-            <h3>Scuttle</h3>
+            <h3>
+                Scuttle<img id="icon" src={icon} alt="icon"></img>
+            </h3>
             <hr></hr>
             {props.children}
             {/* <div className="game-info">
